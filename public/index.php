d7207dc0fc4a5cd80eda1c842ce01a0f175def7b
Asier <asielng2a@gmail.com>
2024-11-13 20:29:16 +0100

    borrar repo

--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Autoload de clases y configuración inicial
-require_once '../src/config/app.php';
-
-// Ruta básica de inicio
-header("Location: creature/index.php");
-exit();
--- src/config/app.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-define('BASE_URL', '/RoleGameApp/public/');
-require_once 'database.php';
